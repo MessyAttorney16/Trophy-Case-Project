2045dbb50cb7688beaf7e631e85d8e484f3cbe3f
Junior553 <70332489+Junior553@users.noreply.github.com>
2022-01-20 10:52:10 -0600

    More Updates

---
+++ TrophyCaseProject/Models/Trophy.cs
@@ -0,0 +1,12 @@
+﻿namespace TrophyCaseProject.Models
+{
+    public class Trophy
+    {
+        public int Id { get; set; }
+        public string? Division { get; set; }
+        public int Year { get; set;}
+        public string? Sport { get; set; }
+        public string? Place { get; set; }
+        public string? Class { get; set; }
+    }
+}
